24d5f557ab1f818b79efedd73ba88535b4a9f2ba
Harshu9996 <mharshsharma211@gmail.com>
2024-03-31 20:42:14 +0530

    Added queryParser, Completed step 3

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,21 @@
-const readCSV = require('../src/csvReader');
+const parseQuery = require('../src/queryParser');
 
-test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
-    expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30');
+test('Parse SQL Query', () => {
+    const query = 'SELECT id, name FROM sample';
+    const parsed = parseQuery(query);
+    expect(parsed).toEqual({
+        fields: ['id', 'name'],
+        table: 'sample'
+    });
+});
+
+//Test case which would throw an error
+
+test('Parse SQL Query', () => {
+    const query = 'SELECT id, height FROM sample';
+    const parsed = parseQuery(query);
+    expect(parsed).toEqual({
+        fields: ['id', 'name'],
+        table: 'sample'
+    });
 });
